add check_weight_derivatives, loss_function_derivative, biases. fix gradient_descent sign mistake

NeuralNetwork.py
@@ -21,6 +21,7 @@ class NeuralNetwork:
 
         # an array that represent the nodes in layers
         layers = [num_inputs] + hidden_layers + [num_outputs]
+        self.layers = layers
 
         # There is a weight matrix between each two layers with size of nodes on both side
         weights = []
@@ -34,13 +35,24 @@ class NeuralNetwork:
             d = np.zeros((layers[i], layers[i+1]))
             weight_derivatives.append(d)
         self.weight_derivatives = weight_derivatives
-
         # There is an activation for each node of each layer
         activation = []
         for i in range(len(layers)):
             a = np.zeros(layers[i])
             activation.append(a)
         self.activation = activation
+        # Each activation has a bias except input
+        biases = []
+        for i in range(1,len(layers)):
+            b = np.random.rand(layers[i])
+            biases.append(b)
+        self.biases = biases
+        # Each bias has a derivative
+        bias_derivatives = []
+        for i in range(1,len(layers)):
+            db = np.zeros(layers[i])
+            bias_derivatives.append(db)
+        self.bias_derivatives = bias_derivatives
 
     def feed_forward(self, inputs):
         '''
@@ -50,7 +62,7 @@ class NeuralNetwork:
         '''
         self.activation[0] = inputs
         for i, weight in enumerate(self.weights):
-            self.activation[i+1] = self.sigmoid(np.inner(weight.T, self.activation[i]))
+            self.activation[i+1] = self.sigmoid(np.inner(weight.T, self.activation[i]) + self.biases[i])
         return self.activation[-1]
 
     def back_propagate(self, loss_function_derivative):
@@ -64,6 +76,8 @@ class NeuralNetwork:
         for i in reversed(range(len(self.weight_derivatives))):
             # delta = dC/da * da/dz
             delta = error * self.sigmoid_derivative(self.activation[i+1])
+            # dC/db = dC/da * da/dz * dx/db [note that dx/db = 1]
+            self.bias_derivatives[i] = delta
             # dC/dw = dC/da * da/dz * dz/dw [note that dz/dw = a]
             self.weight_derivatives[i] = np.outer(self.activation[i], delta)
             # update error for next step
@@ -73,7 +87,9 @@ class NeuralNetwork:
     # update the weights and biases with derivative of loss function
     def gradient_descent(self, learning_rate):
         for i in range(len(self.weights)):
-            self.weights[i] += self.weight_derivatives[i] * learning_rate
+            self.weights[i] -= self.weight_derivatives[i] * learning_rate
+        for i in range(1,len(self.biases)):
+            self.biases[i] -= self.bias_derivatives[i] * learning_rate
 
 
     def train(self, inputs, targets, epochs, learning_rate):
@@ -83,13 +99,12 @@ class NeuralNetwork:
             # loop over all training sets
             for input, target in zip(inputs, targets):
                 output = self.feed_forward(input)
-                loss_function_derivative = 2.0 * (target - output)
-                self.back_propagate(loss_function_derivative)
+                self.back_propagate(self.loss_function_derivative(target, output))
                 self.gradient_descent(learning_rate)
                 sum_errors += self.loss_function(target, output)
 
-            # report mean error of all training sets
-            print("Error: {} at epoch {}".format(sum_errors / len(inputs), i+1))
+            # report mean error of all training sets each 10th epoch
+            print("Error: {:.10f} at epoch {}".format(sum_errors / len(inputs), i+1)) if i%10 == 0 else None
 
 
     def sigmoid(self, x):
@@ -106,5 +121,11 @@ class NeuralNetwork:
         Calculate loss function
         '''
         return np.average((target - output) ** 2)
+
+    def loss_function_derivative(self, target, output):
+        '''
+        Calculate loss function derivative (dC/da)
+        '''
+        return -2.0 * (target - output)
 
 

test_network.py
@@ -2,38 +2,66 @@
 import numpy as np
 from NeuralNetwork import NeuralNetwork
 
+def check_weight_derivatives(network, input, target, epsilon):
+    '''
+    Checks the derivatives of the network by a simple numerical differentiation method
+    input/target are only one set
+    '''
+    #indexes for the address of derivative we check
+    # f(x)
+    output1 = network.feed_forward(inputs=input)
+    loss_function1 = network.loss_function(target, output1)
+    # calculate derivatives once
+    network.back_propagate(network.loss_function_derivative(target, output1))
+    # change the weight by epsilon (x + h)
+    for i in range(len(network.layers) - 1):
+        for j in range(network.layers[i]):
+            for k in range(network.layers[i+1]):
+                network.weights[i][j, k] += epsilon
+                # f(x+h)
+                output2 = network.feed_forward(inputs=input)
+                loss_function2 = network.loss_function(target, output2)
+                # slope = (f(x+h) - f(x)) / h
+                numerical_derivative = (loss_function2 - loss_function1) / epsilon
+                difference = abs(numerical_derivative - network.weight_derivatives[i][j, k])
+
+                if difference > 1e-5:
+                    print()
+                    print(" The difference is:           {:.8f}".format(difference))
+                    print(" The numerical derivative is: {:.8f}".format(numerical_derivative))
+                    print(" The network's derivative is: {:.8f}".format(network.weight_derivatives[i][j, k]))
+                # undo changes
+                network.weights[i][j, k] -= epsilon
+
+
+
+
 if __name__ == "__main__":
 
     # create a dataset to train a network for the sum operation
     inputs = np.random.rand(1000,2) * 0.5
-    targets = inputs[:,0] + inputs[:,1]
+    targets = np.stack((inputs[:,0] + inputs[:,1], inputs[:,0] - inputs[:,1]), axis=1)
 
+    # For testing only one element in output is allowed
+    #targets = inputs[:,0] + inputs[:,1]
+    #network = NeuralNetwork(num_inputs=2, hidden_layers=[5], num_outputs=1)
+    #test_network(network=network, input=inputs[0], target=targets[0], epsilon=1e-10)
+    #exit()
     # create a Multilayer Perceptron with one hidden layer
-    network = NeuralNetwork(num_inputs=2, hidden_layers=[5], num_outputs=1)
-    # first_input = inputs[0]
-    # first_output = targets[0]
-    # output = network.feed_forward(inputs=first_input)
-    # network.back_propagate(2*(output - first_output))
-    # loss_function1 = network.loss_function(target=first_output, output=output)
-    # network.weights[1][2,0] += 1e-10
-    # output2 = network.feed_forward(inputs=first_input)
-    # loss_function2 = network.loss_function(target=first_output, output=output2)
-    # derivative = (loss_function2 - loss_function1) / 1e-10
-    # print(derivative)
-    # print(network.weight_derivatives[1][2,0])
+    network = NeuralNetwork(num_inputs=2, hidden_layers=[5], num_outputs= 2)
 
     #exit()
     # train network
-    network.train(inputs=inputs, targets=targets, epochs=100, learning_rate=0.05)
+    network.train(inputs=inputs, targets=targets, epochs=1000, learning_rate=0.01)
 
     # create dummy data
     input = np.array([0.3, 0.1])
-    target = np.array([0.4])
+    target = np.array([0.4, 0.2])
 
     # get a prediction
     output = network.feed_forward(inputs=input)
 
     print()
-    print("Our network believes that {} + {} is equal to {}".format(input[0], input[1], output[0]))
-
+    print("Our network believes that {} + {} is equal to {:.8f}".format(input[0], input[1], output[0]))
+    print("Our network believes that {} - {} is equal to {:.8f}".format(input[0], input[1], output[1]))
 