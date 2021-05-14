2fc521cffce6cfc188ad0b30615a8a076ef62c3e
naeeme.dmo <naeeme.dmo@googlemail.com>
2021-05-15 00:52:29 +0200

    Add options for google doctring and type hints

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,13 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Add napoleon to the extensions list for Google docstring style
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.mathjax'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,3 +57,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+# for documentation of __init__ in a class
+autodoc_default_options = {
+    'special-members': '__init__',
+}
+
+# multiple returns by google style
+# napoleon_custom_sections = [('Returns', 'params_style')]
