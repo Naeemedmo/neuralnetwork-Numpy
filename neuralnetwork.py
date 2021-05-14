3494692fdf266aa6e9adcaf2bb3bf64e5189a486
naeeme.dmo <naeeme.dmo@googlemail.com>
2021-05-15 00:53:35 +0200

    WIP: update neural network documentation

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,17 +1,26 @@
 import numpy as np
+from typing import Iterator
+from activation_function import ActivationFunction
 
 
 class NeuralNetwork:
+    """ Multi layer perceptron neural network
 
-    def __init__(self, num_inputs, hidden_layers, num_outputs, activation_function):
-        '''
-        Initialize a neural network
-        num_inputs number of input nodes in each training set
-        hidden_layers ndarray[n,m,..]: len(ndarray) = number of hidden layers,
-                                       n, m.. are number of nodes per layer
-        num_outputs number of output nodes
-        activation_function is "sigmoid" or "tanh"
-        '''
+    """
+    def __init__(self,
+                 num_inputs: int,
+                 hidden_layers: list[int],
+                 num_outputs: int,
+                 activation_function: ActivationFunction()
+                 ):
+        """ Initializes a neural network
+
+        Args:
+            num_inputs: number of input nodes
+            hidden_layers: number of nodes in each hidden layer
+            num_outputs: number of output nodes
+            activation_function: See ActivationFunction class description
+        """
         self.num_inputs = num_inputs
         self.hidden_layers = hidden_layers
         self.num_outputs = num_outputs
@@ -28,13 +37,16 @@ class NeuralNetwork:
         # Each activation has a bias except input
         self.biases = [np.random.rand(layer) for layer in layers[1:]]
 
-    def feed_forward(self, inputs):
-        '''
-        Calculate the activation for each node with the help of weights
-        activation(L+1) = sigmoid(weights(L)*activation(L) + biases(L+1))
-                        or   tanh(weights(L)*activation(L) + biases(L+1))
-        returns activations for each node
-        '''
+    def feed_forward(self, inputs: np.ndarray) -> list[np.ndarray]:
+        """Calculates the activation for each node.
+
+        Args:
+            inputs: numpy array of shape (batch_size, input nodes)
+
+        Returns:
+            a list with size of number of layers,
+            each element is a numpy array of shape (batch_size, number of nodes)
+        """
         # There is an activation for each node of each layer
         activations = [np.atleast_2d(inputs)]
         for bias, weight in zip(self.biases, self.weights):
@@ -43,18 +55,35 @@ class NeuralNetwork:
             )
         return activations
 
-    def predict(self, inputs):
-        '''
-        return the prediction for given input
-        '''
+    def predict(self, inputs: np.ndarray) -> list[np.ndarray]:
+        """ Predicts the output for a given input
+
+        Args:
+            inputs: numpy array of shape (batch_size, number of input nodes)
+
+        Returns:
+            the prediction (activations of the last layer),
+            a list with a numpy array of shape (batch_size, number of output nodes)
+        """
         activations = self.feed_forward(inputs)
         return activations[-1]
 
-    def back_propagate(self, target, activations):
-        '''
-        Loss(target, activation) = Σ(target-activation)**2
-        Apply chain rule to get derivative
-        '''
+    def back_propagate(self,
+                       target: np.ndarray,
+                       activations: list[np.ndarray]
+                       ) -> tuple[list[np.ndarray], list[np.ndarray]]:
+        """ Calculates the weights and biases for the network
+
+        Args:
+            target: numpy array of shape (batch_size, number of output nodes)
+            activations: returned by :meth:`feed_forward`
+
+        Returns:
+            a tuple containing two 2D numpy arrays
+
+                - :math:`\\frac{\\partial L}{\\partial w}` (:math:`L` loss function, :math:`w` weights)
+                - :math:`\\frac{\\partial L}{\\partial b}` (:math:`L` loss function, :math:`b` biases)
+        """
         # Each bias has a derivative
         bias_derivatives = self.biases.copy()
         # There is a derivative for each weight
@@ -75,14 +104,46 @@ class NeuralNetwork:
             error = np.inner(self.weights[i], delta).T
         return weight_derivatives, bias_derivatives
 
-    # update the weights and biases with derivative of loss function
-    def gradient_descent(self, learning_rate, weight_derivatives, bias_derivatives):
+    def gradient_descent(self,
+                         learning_rate: float,
+                         weight_derivatives: list[np.ndarray],
+                         bias_derivatives: list[np.ndarray]
+                         ):
+        """ Updates the weights and biases with derivative of loss function
+
+        Args:
+            learning_rate: learning rate of the gradient descent
+            weight_derivatives: derivative of loss function with respect to weights
+            bias_derivatives: derivative of loss function with respect to biases
+
+        """
         for i in range(len(self.weights)):
             self.weights[i] -= weight_derivatives[i] * learning_rate
         for i in range(len(self.biases)):
             self.biases[i] -= bias_derivatives[i] * learning_rate
 
-    def train(self, inputs, targets, epochs, learning_rate, batch_size):
+    def train(self,
+              inputs: np.ndarray,
+              targets: np.ndarray,
+              epochs: int,
+              learning_rate: float,
+              batch_size: int
+              ) -> Iterator[float]:
+        """ Trains the neural network
+
+            Args:
+                inputs: 3D array of input nodes
+                targets: 3D array of target nodes
+                epochs: number of iterations for training
+                learning_rate: learning rate of the gradient descent
+                batch_size: number of training sets for each traning.
+                            batch_size = 1 -> stochastic gradient descent
+                            batch_size < traning set -> mini-batch gradient descent
+                            batch_size = traning set -> batch gradient descent
+            Yields:
+                mean loss function per epoch
+
+        """
         for i in range(epochs):
             sum_errors = 0.0
             # shuffle the training sets first: stochastic gradient descent
@@ -102,14 +163,33 @@ class NeuralNetwork:
                 sum_errors += self.loss_function(shuffled_targets[j:k], activations[-1])
             yield sum_errors / len(shuffled_inputs)
 
-    def loss_function(self, target, output):
-        '''
-        Calculate loss function
-        '''
+    def loss_function(self,
+                      target: np.ndarray,
+                      output: np.ndarray
+                      ) -> np.ndarray:
+        """ Calculates loss function
+
+        Args:
+            target: 3D array of target nodes
+            output: 3D array of output nodes, networks prediction
+
+        Returns:
+            An array of loss function values for target and output
+        """
         return np.average((target - output) ** 2)
 
-    def loss_function_derivative(self, target, output):
-        '''
-        Calculate loss function derivative (dC/da)
-        '''
+    def loss_function_derivative(self,
+                                 target: np.ndarray,
+                                 output: np.ndarray
+                                 ) -> np.ndarray:
+        """ Calculates loss function derivative with respect to output
+
+        Args:
+            target: 3D array of target nodes
+            output: 3D array of output nodes, networks prediction
+
+        Returns:
+            An array of loss function derivatives for target and output
+
+        """
         return -2.0 * (target - output)
