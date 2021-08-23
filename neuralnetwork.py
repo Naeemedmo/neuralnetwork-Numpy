5cde3422577dff666d8a061bb88cf1bbc88ac409
naeeme.dmo <naeeme.dmo@googlemail.com>
2021-08-23 14:38:25 +0200

    Add docstring for neuralnetwork module

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -16,10 +16,10 @@ class NeuralNetwork:
         """ Initializes a neural network
 
         Args:
-            num_inputs: number of input nodes
-            hidden_layers: number of nodes in each hidden layer
-            num_outputs: number of output nodes
-            activation_function: See ActivationFunction class description
+            num_inputs: number of input neurons
+            hidden_layers: number of neurons in each hidden layer
+            num_outputs: number of output neurons
+            activation_function: :math:`\\sigma(x)` See ActivationFunction class description
         """
         self.num_inputs = num_inputs
         self.hidden_layers = hidden_layers
@@ -27,27 +27,33 @@ class NeuralNetwork:
         # choice of activation function
         self.activation_function = activation_function
 
-        # an array that represent the nodes in layers
+        # an array that represent the neurons in layers
         layers = [num_inputs] + hidden_layers + [num_outputs]
         self.layers = layers
 
-        # There is a weight matrix between each two layers with size of nodes on both side
+        # There is a weight matrix between each two layers with size of neurons on both side
         self.weights = [np.random.rand(layers[i], layers[i + 1]) for i in range(len(layers) - 1)]
 
         # Each activation has a bias except input
         self.biases = [np.random.rand(layer) for layer in layers[1:]]
 
     def feed_forward(self, inputs: np.ndarray) -> list[np.ndarray]:
-        """Calculates the activation for each node.
+        """ Calculates the activation for each neurons in the whole network:
+
+        :math:`a_{i} = \\sigma(a_{i-1}.w_i + b_i)`
+
+        where :math:`\\sigma` is the activation function and :math:`a_{i}` is the activations
+        for layer i, :math:`w_i` and :math:`b_i` are weights and biases between layer i-1 and i
 
         Args:
-            inputs: numpy array of shape (batch_size, input nodes)
+            inputs: an m-by-n matrix, where m is the batch size and n is the number of input
+            neurons
 
         Returns:
-            a list with size of number of layers,
-            each element is a numpy array of shape (batch_size, number of nodes)
+            a list of m-by-n matrices, where m is the batch size and n is the number of neurons of
+            each layer
         """
-        # There is an activation for each node of each layer
+        # There is an activation for each neurons of each layer
         activations = [np.atleast_2d(inputs)]
         for bias, weight in zip(self.biases, self.weights):
             activations.append(
@@ -56,14 +62,15 @@ class NeuralNetwork:
         return activations
 
     def predict(self, inputs: np.ndarray) -> list[np.ndarray]:
-        """ Predicts the output for a given input
+        """ Predicts the output (:math:`a_{i}`) for a given input (:math:`a_{0}`)
 
         Args:
-            inputs: numpy array of shape (batch_size, number of input nodes)
+            inputs: an m-by-n matrix, where m is the batch size and n is the number of input
+            neurons
 
         Returns:
-            the prediction (activations of the last layer),
-            a list with a numpy array of shape (batch_size, number of output nodes)
+            the prediction (activations of the last layer), which is an m-by-n matrix, where m is
+            the batch size and n is the number of output neurons
         """
         activations = self.feed_forward(inputs)
         return activations[-1]
@@ -75,14 +82,18 @@ class NeuralNetwork:
         """ Calculates the weights and biases for the network
 
         Args:
-            target: numpy array of shape (batch_size, number of output nodes)
+            target: an m-by-n matrix, where m is the batch size and n is the number of output
+            neurons
             activations: returned by :meth:`feed_forward`
 
         Returns:
             a tuple containing two 2D numpy arrays
 
-                - :math:`\\frac{\\partial L}{\\partial w}` (:math:`L` loss function, :math:`w` weights)
-                - :math:`\\frac{\\partial L}{\\partial b}` (:math:`L` loss function, :math:`b` biases)
+                - :math:`\\frac{\\partial L}{\\partial w}` \
+                (:math:`L` loss function, :math:`w` weights)
+
+                - :math:`\\frac{\\partial L}{\\partial b}` \
+                (:math:`L` loss function, :math:`b` biases)
         """
         # Each bias has a derivative
         bias_derivatives = self.biases.copy()
@@ -113,8 +124,8 @@ class NeuralNetwork:
 
         Args:
             learning_rate: learning rate of the gradient descent
-            weight_derivatives: derivative of loss function with respect to weights
-            bias_derivatives: derivative of loss function with respect to biases
+            weight_derivatives: :math:`\\frac{\\partial L}{\\partial w}`
+            bias_derivatives: :math:`\\frac{\\partial L}{\\partial b}`
 
         """
         for i in range(len(self.weights)):
@@ -132,16 +143,18 @@ class NeuralNetwork:
         """ Trains the neural network
 
             Args:
-                inputs: 3D array of input nodes
-                targets: 3D array of target nodes
+                inputs: an m-by-n matrix, where m is number of traning sets and n is number of
+                inputs per set
+                targets: an m-by-n matrix, where m is number of training sets and n is number of
+                outputs per set
                 epochs: number of iterations for training
                 learning_rate: learning rate of the gradient descent
-                batch_size: number of training sets for each traning.
-                            batch_size = 1 -> stochastic gradient descent
-                            batch_size < traning set -> mini-batch gradient descent
-                            batch_size = traning set -> batch gradient descent
+                batch_size: number of training sets for each traning \n
+                            batch_size = 1 (stochastic gradient descent) \n
+                            batch_size < traning set (mini-batch gradient descent) \n
+                            batch_size = traning set (batch gradient descent)
             Yields:
-                mean loss function per epoch
+                mean loss function value per epoch
 
         """
         for i in range(epochs):
@@ -152,13 +165,16 @@ class NeuralNetwork:
             shuffled_targets = targets[permutation]
             n_batch = len(shuffled_inputs) // batch_size
             if n_batch * batch_size != len(shuffled_inputs):
-                exit(" Input array is not dividable by batch_size! Please choose another batch_size!")
+                exit(" Input array is not dividable by batch_size!\
+                    Please choose another batch_size!")
             # loop over all training sets
             for n in range(n_batch):
                 j = n * batch_size
                 k = (n + 1) * batch_size
                 activations = self.feed_forward(shuffled_inputs[j:k])
-                weight_derivatives, bias_derivatives = self.back_propagate(shuffled_targets[j:k], activations)
+                weight_derivatives, bias_derivatives = self.back_propagate(
+                    shuffled_targets[j:k], activations
+                    )
                 self.gradient_descent(learning_rate, weight_derivatives, bias_derivatives)
                 sum_errors += self.loss_function(shuffled_targets[j:k], activations[-1])
             yield sum_errors / len(shuffled_inputs)
@@ -166,15 +182,17 @@ class NeuralNetwork:
     def loss_function(self,
                       target: np.ndarray,
                       output: np.ndarray
-                      ) -> np.ndarray:
-        """ Calculates loss function
+                      ) -> float:
+        """ Calculates loss function :math:`L = \\frac{1}{n}\\sum_{i=1}^{n}(y_i-\\hat y_i)^2`
 
         Args:
-            target: 3D array of target nodes
-            output: 3D array of output nodes, networks prediction
+            target: target values, an m-by-n matrix, where m is the batch size and n is the number
+            of output neurons (:math:`y_i`)
+            output: network predicted values, an m-by-n matrix, where m is the batch size and n is
+            the number of output neurons (:math:`\\hat y_i`)
 
         Returns:
-            An array of loss function values for target and output
+            Averaged loss function value for targets and outputs
         """
         return np.average((target - output) ** 2)
 
@@ -182,14 +200,17 @@ class NeuralNetwork:
                                  target: np.ndarray,
                                  output: np.ndarray
                                  ) -> np.ndarray:
-        """ Calculates loss function derivative with respect to output
+        """ Calculates loss function derivative
+        :math:`\\frac{\\partial L}{\\partial \\hat y} = -2.0\\times(y_i-\\hat y_i)`
 
         Args:
-            target: 3D array of target nodes
-            output: 3D array of output nodes, networks prediction
+            target: target values, an m-by-n matrix, where m is the batch size and
+            n is the number of output neurons (:math:`y_i`)
+            output: network predicted values, an m-by-n matrix, where m is the batch
+            size and n is the number of output neurons (:math:`\\hat y_i`)
 
         Returns:
-            An array of loss function derivatives for target and output
+            An m-by-n matrix of loss function derivatives for target and output
 
         """
         return -2.0 * (target - output)
