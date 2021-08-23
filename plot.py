4bd822da1fd0b82739fc798ca3d6dbeec5530940
naeeme.dmo <naeeme.dmo@googlemail.com>
2021-08-23 15:58:50 +0200

    Add docstring for plot module

--- plot.py
+++ plot.py
@@ -4,13 +4,25 @@ from matplotlib.ticker import MaxNLocator
 
 
 class LossPlot():
-    '''
-        Left subplot: plot averaged loss function over iterations
-    '''
-    def __init__(self, fig):
+    """ Plots averaged loss function over iterations (Left subplot)
+    """
+    def __init__(self, fig: plt.figure):
+        """ Initialize subplot
+
+        Args:
+            fig: matplotlib figure
+        """
         self.ax = fig.add_subplot(1, 2, 1)
 
-    def plot(self, data, test_data, pause):
+    def plot(self, data: list, test_data: list, pause: float):
+        """ Plots the training and testing error and pause
+
+        Args:
+            data: training data error
+            test_data: testing data error
+            pause: pause between plots
+
+        """
         self.ax.clear()
         self.ax.set_title('Mean loss function over iterations')
         self.ax.set_xlabel('Iterations')
@@ -24,18 +36,19 @@ class LossPlot():
 
 
 class NetworkPlot():
-    '''
-        Right subplot: draw the network neurons and connections.
-        The linewidth of line represent the effect of weights for each iteration
-        The marker on the nodes represent the effect of biases for each iteration
-        For both blueish color is + and the other ones negative
-    '''
+    """ Draws the network neurons and connections (Right subplot) \n
+    The linewidth represent the effect of weights in each iteration.
+    The marker on the neurons represent the effect of biases in each iteration.
+    For both blueish color is positive and the redish color is negative values.
+    For plot properties, such as radius of the neurons and the minimum/maximum of linewidth,
+    please look at the source code.
+    """
     # fixed size for figure
     h_max = 12.0
     v_max = 12.0
     side_space = 1.0
     top_space = 1.0
-    # radius for the nodes
+    # radius for the neurons
     radius = 0.35
     # line thickness of weights
     max_linewidth = 3.0
@@ -44,7 +57,13 @@ class NetworkPlot():
     max_markersize = 4
     min_markersize = 2
 
-    def __init__(self, fig):
+    def __init__(self, fig: plt.figure):
+        """ Initialize subplot \n
+        Initializes the minimum and maximum values for the weights and biases.
+
+        Args:
+            fig: matplotlib figure
+        """
         self.ax = fig.add_subplot(1, 2, 2)
         # initialize the min/max
         self.max_weights = -float("inf")
@@ -52,27 +71,60 @@ class NetworkPlot():
         self.max_biases = -float("inf")
         self.min_biases = float("inf")
 
-    def iteration_bounds(self, weights, biases):
+    def iteration_bounds(
+            self, weights: list[np.ndarray], biases: list[np.ndarray]
+            ) -> tuple[float, float, float, float]:
+        """ Finds the minimum and maximum of weights and biases for an iteration
+
+        Args:
+            weights: list of m-by-n matrices, containing the weights between neurons of the neural network
+            biases: list of 1-by-n matrices, containing the biases for each neuron of the neural network
+
+        Returns:
+            a tuple containing bounds of weights and biases
+            :math:`w_{min}, w_{max}, b_{min}, b_{max}`
+        """
         max_weights = np.max([np.max(np.abs(w)) for w in weights])
         min_weights = np.min([np.min(np.abs(w)) for w in weights])
         max_biases = np.max([np.max(np.abs(b)) for b in biases])
         min_biases = np.min([np.min(np.abs(b)) for b in biases])
         return min_weights, max_weights, min_biases, max_biases
 
-    def update_bounds(self, weights, biases):
+    def update_bounds(self, weights: list[np.ndarray], biases: list[np.ndarray]):
+        """ Updates the minimum and maximum of weights and biases regarding the current iteration
+
+        Args:
+            weights: list of m-by-n matrices, containing the weights between neurons of the neural network
+            biases: list of 1-by-n matrices, containing the biases for each neuron of the neural network
+
+        """
         min_weights, max_weights, min_biases, max_biases = self.iteration_bounds(weights, biases)
         self.max_weights = max(self.max_weights, max_weights)
         self.min_weights = min(self.min_weights, min_weights)
         self.max_biases = max(self.max_biases, max_biases)
         self.min_biases = min(self.min_biases, min_biases)
 
-    def draw_nodes_of_layer(self, i_layer, num_nodes, radius):
-        for n in range(num_nodes):
+    def draw_neurons_of_layer(self, i_layer: int, num_neurons: int, radius: float):
+        """ Draws all the neurons in a layer
+
+        Args:
+            i_layer: index of the layer
+            num_neurons: number of neurons on this layer
+            radius: radius of neuron on the subplot
+        """
+        for n in range(num_neurons):
             circle = plt.Circle((self.h_levels[i_layer], self.v_levels[i_layer][n]),
                                 radius=radius, color='#F5DD90', zorder=100)
             self.ax.add_patch(circle)
 
-    def draw_weights_of_layer_leftlayer(self, i_layer, weights):
+    def draw_weights_of_layer_leftlayer(self, i_layer: int, weights: list[np.ndarray]):
+        """ Draws all the connections between the neurons of the current layer and leftside neurons
+
+        Args:
+            i_layer: index of the layer
+            weights: list of m-by-n matrices, containing the weights between neurons of the neural network
+
+        """
         for n in range(self.layers[i_layer]):
             for nn in range(self.layers[i_layer-1]):
                 weight = abs(weights[i_layer-1][nn][n])
@@ -82,8 +134,15 @@ class NetworkPlot():
                 line_color = '#586BA4' if weights[i_layer-1][nn][n] > 0.0 else '#F76C5E'
                 plt.plot(x_values, y_values, zorder=1, color=line_color, linewidth=linewidth)
 
-    def draw_biases_of_layer(self, i_layer, num_nodes, biases):
-        for n in range(num_nodes):
+    def draw_biases_of_layer(self, i_layer: int, num_neurons: int, biases: list[np.ndarray]):
+        """ Draws a marker (square) for each bias on the neurons of the current layer
+
+        Args:
+            i_layer: index of the layer
+            num_neurons: number of neurons on the layer
+            biases: list of 1-by-n matrices, containing the biases for each neuron of the neural network
+        """
+        for n in range(num_neurons):
             marker_size = self.m_marker_b * (biases[i_layer][n] - self.min_biases) + self.min_markersize
             marker_color = '#324376' if biases[i_layer][n] > 0.0 else '#F68E5F'
             plt.plot(
@@ -95,7 +154,15 @@ class NetworkPlot():
                 markersize=marker_size**2
             )
 
-    def plot(self, layers, weights, biases, pause):
+    def plot(self, layers: list, weights: list[np.ndarray], biases: list[np.ndarray], pause: float):
+        """ Plots the whole neural network for an iteration
+
+        Args:
+            layers: number of neurons in each hidden layer
+            weights: list of m-by-n matrices, containing the weights between neurons of the neural network
+            biases: list of 1-by-n matrices, containing the biases for each neuron of the neural network
+            pause: pause between plots
+        """
 
         # update the min, max of weights and biases
         self.update_bounds(weights, biases)
@@ -109,32 +176,32 @@ class NetworkPlot():
         self.ax.set_ylim((0.0, self.v_max))
 
         # get information on layers for figure
-        num_nodes_height = max(self.layers)  # layer with the most nodes define height
-        num_nodes_width = len(self.layers)  # number of layers define width
+        num_neurons_height = max(self.layers)  # layer with the most neurons define height
+        num_neurons_width = len(self.layers)  # number of layers define width
 
         h_center = self.h_max / 2.0
         v_center = self.v_max / 2.0
-        # spacing between nodes
-        h_space = (self.h_max - 2 * self.side_space) / (num_nodes_width - 1)
-        v_space = (self.v_max - 2 * self.top_space) / (num_nodes_height - 1)
+        # spacing between neurons
+        h_space = (self.h_max - 2 * self.side_space) / (num_neurons_width - 1)
+        v_space = (self.v_max - 2 * self.top_space) / (num_neurons_height - 1)
         # layers horizontal position
-        h_start = h_center - ((num_nodes_width - 1) / 2) * h_space
+        h_start = h_center - ((num_neurons_width - 1) / 2) * h_space
         self.h_levels = [h_start + n * h_space for n in range(len(self.layers))]
-        # vertical position of each node
+        # vertical position of each neuron
         self.v_levels = [np.zeros((layer)) for layer in self.layers]
-        for layer, num_nodes in enumerate(self.layers):
-            # calculate position of the toppest node
-            v_start = v_center + ((num_nodes - 1) / 2) * v_space
-            self.v_levels[layer] = [v_start - n * v_space for n in range(num_nodes)]
+        for layer, num_neurons in enumerate(self.layers):
+            # calculate position of the toppest neuron
+            v_start = v_center + ((num_neurons - 1) / 2) * v_space
+            self.v_levels[layer] = [v_start - n * v_space for n in range(num_neurons)]
 
-        # first make the input nodes
-        self.draw_nodes_of_layer(i_layer=0, num_nodes=self.layers[0], radius=self.radius)
-        for layer, num_nodes in enumerate(self.layers[1:]):
+        # first make the input neurons
+        self.draw_neurons_of_layer(i_layer=0, num_neurons=self.layers[0], radius=self.radius)
+        for layer, num_neurons in enumerate(self.layers[1:]):
             # first markers for biases
             self.draw_biases_of_layer(layer, self.layers[layer+1], biases)
             layer += 1
-            self.draw_nodes_of_layer(i_layer=layer, num_nodes=self.layers[layer], radius=self.radius)
-            # connect the node to the previous layer
+            self.draw_neurons_of_layer(i_layer=layer, num_neurons=self.layers[layer], radius=self.radius)
+            # connect the neuron to the previous layer
             self.draw_weights_of_layer_leftlayer(i_layer=layer, weights=weights)
         plt.axis('off')
         # print information on the corner
